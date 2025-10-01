bc2ed04d3219c81895f57e2d66258195464f8cda
Aditya Tapadar <adityatapadar2017@gmail.com>
2025-10-01 19:29:26 +0530

    updated form

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,12 +1,15 @@
-"use client"
+"use client";
 
-import React, { useEffect } from 'react';
-import Navbar from '@/components/Navbar';
-import Sidebar from '@/components/Sidebar';
-import StoreProvider, { useAppSelector } from './redux';
+import React, { useEffect } from "react";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import AuthProvider from "./authProvider";
+import StoreProvider, { useAppSelector } from "./redux";
 
-const DashboardLayout = ({children} : {children: React.ReactNode}) => {
-  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
   useEffect(() => {
@@ -18,21 +21,26 @@ const DashboardLayout = ({children} : {children: React.ReactNode}) => {
   });
 
   return (
-    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-        <Sidebar/>
-        <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50
-          ${ isSidebarCollapsed ? "" : 'md:pl-64'}`}>
-            <Navbar />
-            {children}
-        </main>
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      <Sidebar />
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+          isSidebarCollapsed ? "" : "md:pl-64"
+        }`}
+      >
+        <Navbar />
+        {children}
+      </main>
     </div>
   );
 };
 
-const DashboardWrapper = ({children} : { children: React.ReactNode }) => {
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      <DashboardLayout>{children}</DashboardLayout>
+      <AuthProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+      </AuthProvider>
     </StoreProvider>
   );
 };
