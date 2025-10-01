3f8720b8bf016701f1099263c8d68d9be962230a
Aditya Tapadar <adityatapadar2017@gmail.com>
2025-10-01 16:46:07 +0530

    redirect images to s3

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,9 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
   eslint: {
     // ✅ Allow production builds to complete even with ESLint errors
     ignoreDuringBuilds: true,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "crewplan-s3-images.s3.us-east-1.amazonaws.com", // <-- your bucket name + region
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
